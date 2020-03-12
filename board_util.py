e96d86e0659e2e528b3a2983ffdc481d5311480b
Kálmán, László <kalman.laszlo@nytud.mta.hu>
2020-03-12 12:04:36 +0100

    added tk_basics

--- board_util.py
+++ board_util.py
@@ -44,3 +44,87 @@ def is_move_possible( board, player, fromPoint, toPoint ):
     ## temporary:
     return True
 
+## This is the hardest one:
+class TodoElement:
+    ## missing!
+    pass
+
+def legitimate_moves( board, player, die1, die2 ):
+    """
+    Return a list of move sequences that are legitimate.
+    Unfortunately, in some cases we have to calculate all
+    legitimate move sequences in order to decide if a given 
+    move sequence is feasible, because of the maximality
+    criterion (the maximum possible distance must be made).
+    """
+    ## Algorithm: We will set up an empty list of move
+    ## sequences in which we will collect the legitimate
+    ## sequences.
+    
+    ## We will also set up a ``todo list'' containing
+    ## `TodoElement` instances, containing a move sequence,
+    ## a board (situation), and a list of remaining rolls.
+    ## The move sequence is a sequence of planned moves,
+    ## and the remaining roll is what numbers we still have
+    ## based on the roll.  Initially, the todo list will
+    ## contain one or two elements, with the move sequence
+    ## being empty (no planned moves so far);  if `die1` and
+    ## `die2` are different numbers, we will have two elements,
+    ## with `remaining-roll` being different depending on the
+    ## order of dice;  if the roll is doubles, we have only
+    ## one triple, the remaining rolls containing four times the
+    ## number rolled.
+
+    ## Then we will go through the todo list, and try to
+    ## produce new sequences from each element; if we have
+    ## finished a sequence (because there are no remaining
+    ## rolls or because no further move is possible), we append
+    ## the result to our list of finished sequences;  otherwise,
+    ## we create an updated version of the current todo element,
+    ## and append the result to the end of our todo list:
+    ## we will get there later on.
+    
+    ## Finally, we could just return the list of finished move
+    ## sequences, but we don't: we first have to keep only the
+    ## maximal ones, because that's the rule in backgammon.
+
+    finished = []
+    if die1 == die2:
+        todo = \
+            [                   # list of todo elements
+                [               # first and only element:
+                    TodoElement( board, [die1, die1, die1, die1] )
+                ]
+            ]                                # end of todo list
+    else:
+        todo = \
+            [                   # list of todo elements
+                [               # first element:
+                    TodoElement( board, [die1, die2] )
+                ],               # end of first element
+
+                [               # second element:
+                    TodoElement( board, [die2, die1] )
+                ]                # end of second element
+            ]                    # end of todo list
+    next_todo_index = 0          # our position in the todo list
+    while True:                  # only stop if nothing more to do
+        if next_todo_index == len( todo ) - 1: # nothing more to do
+            break
+        else:
+            ## missing:
+            ## Try to continue `move-sequence` with first element
+            ## of `remaining-roll`.
+            ## Append result to `finished` if failure or no more
+            ## elements in `remaining-roll`;  append result to
+            ## `todo` if successful.
+
+            ## Prepare for next todo element:
+            next_todo_index += 1
+    ## We will choose the maximal ones in a separate function:
+    return maximalMoveSequences( finished )
+
+def maximalMoveSequences( moveSequences ):
+    ## temporary:
+    return moveSequences
+
---
+++ tk_basics.py
@@ -0,0 +1,37 @@
+## import graphics module
+import tkinter as tk
+
+## Setting up a _root window_, in which everything will appear,
+## e.g., like this:
+root = tk.Tk() # `Tk()` is a constructor exported by `tkinter`
+
+## maximize window:
+screenheight = root.winfo_screenheight()
+screenwidth = root.winfo_screenwidth()
+root.geometry( "%dx%d" % (screenwidth, screenheight) )
+
+## Add a frame to the root window just for the fun of it:
+frame = tk.Frame( root )  # the parameter is the _parent_ in which
+                          # it will appear
+## We must indicate how the frame must appear (using a
+## _geometry manager_), otherwise it wil not appear at all:
+frame.pack()
+
+## add a button to the frame window (of course, `text` is a
+## useful keyword parameter when we use a button)"
+quitButton = tk.Button( frame, text="Quit" )
+
+## You must use a geometry manager for the button to appear,
+## let us use the `grid()` manager within the frame window:
+## place the button into the top left corner:
+quitButton.grid( row=0, column=0 )
+
+## define an event handler of the Quit button:
+def quit_handler( event ):
+    event.widget.quit()   # look up event's widget
+
+## bind the handler to the widget, with event type "left click":
+quitButton.bind( "<Button-1>", quit_handler )
+
+## The end of your main procedure:
+root.mainloop()                 # that's it, nothing else to do
